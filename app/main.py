95ffe0f98601f7975afbe5d32f56bcebb231edaa
JohnLouisMaker <jluisilvenancio@gmail.com>
2026-07-21 23:43:19 -0300

    refactor: Configura CORS para o frontend

--- app/main.py
+++ app/main.py
@@ -7,23 +7,25 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="API Python com FastAPI")
 
-#  CONFIG CORS MIDDLEWARE
+# Lista de origens fixas (como localhost)
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "https://deliver-frontend-three.vercel.app",
+]
+
+# CONFIG CORS MIDDLEWARE
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-        "https://deliver-frontend-three.vercel.app",
-        "https://deliver-frontend-git-main-john-louis-projects-2eeb8231.vercel.app",
-    ],
+    allow_origins=origins,
+    allow_origin_regex=r"https://deliver-frontend-.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 # INCLUINDO ROTAS
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
