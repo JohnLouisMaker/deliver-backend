867885a0a26d76d7ebb556d7738336899f93cd34
JohnLouisMaker <jluisilvenancio@gmail.com>
2026-07-21 23:06:35 -0300

    Teste com seed.py

--- app/seed.py
+++ app/seed.py
@@ -1,9 +1,11 @@
-from app.database.database import SessionLocal
+from app.database.database import SessionLocal, engine
 from app.database.product import CATALOGO_PADRAO
-from app.models.models import ItemCardapio
+from app.models.models import Base, ItemCardapio
 
 
 def seed_db():
+    Base.metadata.create_all(bind=engine)
+
     with SessionLocal() as db:
         if db.query(ItemCardapio).count() > 0:
             print("CARDÁPIO JÁ EXISTENTE - Nenhuma alteração feita.")
@@ -11,11 +13,9 @@ def seed_db():
 
         print("Semeando o cardápio no banco de dados...")
         try:
-
             novos_itens = [ItemCardapio(**item) for item in CATALOGO_PADRAO]
             db.add_all(novos_itens)
             db.commit()
-            
             print(f"{len(novos_itens)} itens inseridos com sucesso!")
         except Exception as e:
             db.rollback()
